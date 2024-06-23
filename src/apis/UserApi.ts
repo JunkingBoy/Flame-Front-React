ebcb7f69da4b168ed3d5b28a14a9d0bbacfbb4b1
junkingboy <junkingboy@163.com>
2024-06-23 15:12:20 +0800

    对接后端登录注册api

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -3,43 +3,16 @@ import { LoginResponse, RegisterResponse, UserBugInfoResponse, UserCarDataRespon
 import { ProgramReponse, ProgramBugDetailResponse } from "../interface/ProgramInterface";
 
 async function login(username: string, password: string): Promise<LoginResponse> {
-    // // 密钥初始化
-    // await initEncryptionKey();
-    // // 加密密码
-    // let {ciphertext, iv} = await encryptData(password);
-
-    // // 加密后数据转为base64
-    // let encryptedPasswordBase64 = btoa(
-    //     Array.from(new Uint8Array(ciphertext))
-    //         .map(byte => String.fromCharCode(byte))
-    //         .join('')
-    // );
-
-    // let ivBase64 = btoa(
-    //     Array.from(new Uint8Array(iv))
-    //         .map(byte => String.fromCharCode(byte))
-    //         .join('')
-    // );
-
     let response: any = await axiosInstance.post(`${BASE_URL}/user/login`, {
         phone: username,
         password: password,
-        // iv: ivBase64,
     });
     
     return response.data;
 }
 
 async function register(phone: number, password: string, confirmPassword: string): Promise<RegisterResponse> {
-    console.log(password);
-    console.log(confirmPassword);
-    // let utf8password: string = new TextEncoder().encode(password).toString();
-    // let utf8ConfPassword: string = new TextEncoder().encode(confirmPassword).toString();
-
-    // console.log(utf8password);
-    // console.log(utf8ConfPassword);
-
-    let response: any = await axiosInstance.post(`${BASE_URL}/user/register`, {
+    let response: any = await axiosInstance.put(`${BASE_URL}/user/register`, {
         phone: phone,
         password: password,
         password_confirm: confirmPassword
