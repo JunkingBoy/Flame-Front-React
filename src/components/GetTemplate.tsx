18d1359420d9eb1192b7c0cb8e1ea4870a062c3e
junkingboy <junkingboy@163.com>
2024-06-26 02:17:41 +0800

    完善Project页面.新增该页面相关组件

---
+++ src/components/GetTemplate.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { DownOutlined } from '@ant-design/icons';
+import type { DropdownProps, MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+
+import { getTemplate } from '../apis/UpDown';
+
+const GetTemplate: React.FC = () => {
+    let [open, setOpen] = useState(false);
+
+    // 这里发送请求到后端获取对应的用例模板
+    const handleMenuClick: MenuProps['onClick'] = async (e) => {
+        if (e.key === '1') {
+            setOpen(false);
+            await getTemplate(e.key);
+        }
+    };
+
+    const handleOpenChange: DropdownProps['onOpenChange'] = (nextOpen, info) => {
+        if (info.source === 'trigger' || nextOpen) {
+            setOpen(nextOpen);
+        }
+    };
+
+    const items: MenuProps['items'] = [
+    {
+        label: '功能测试用例模板',
+        key: '1',
+    },
+    {
+        label: '接口测试用例模板',
+        key: '2',
+    }
+    ];
+
+    return (
+        <Dropdown
+            menu={{
+                items,
+                onClick: handleMenuClick,
+            }}
+            onOpenChange={handleOpenChange}
+            open={open}
+            >
+            <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                获取用例模板
+                <DownOutlined />
+                </Space>
+            </a>
+        </Dropdown>
+    );
+}
+
+export default GetTemplate;
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,107 @@
-/*
- * @Author: Lucifer
- * @Data: Do not edit
- * @LastEditors: Lucifer
- * @LastEditTime: 2024-06-25 16:15:47
- * @Description: 
- */
-import React from 'react'
+import React from 'react';
+import { Typography } from 'antd';
+
+import {
+    mainDiv,
+    inlineHeaderDiv,
+    inlineContentDiv,
+    inlineContentInlineDiv
+} from './Project.module';
+
+import ClickForm from './ClickForm';
+
+import SearchSelf from './Search';
+
+const { Title } = Typography;
+
+const headerLeft: React.CSSProperties = {
+    width: '75%',
+    height: '30%',
+    textAlign: 'center'
+}
+
+const headerRight: React.CSSProperties = {
+    width: '25%',
+    height: '30%',
+    textAlign: 'center'
+}
+
+const contentLeft: React.CSSProperties = {
+    width: '75%',
+    height: '70%'
+}
+
+const inlineContentLeft: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '98%',
+    height: '100%',
+    margin: '20px 0 12px 20px',
+    borderRadius: '8px',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.3)'
+}
+
+const contentRight: React.CSSProperties = {
+    width: '25%',
+    height: '70%',
+}
+
+const inlineContentRight: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '85%',
+    height: '100%',
+    margin: '20px 0 12px 20px',
+    borderRadius: '8px',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.3)'
+}
+
+const inlineContentInlineLeft: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    height: '100%',
+}
 
 const Project: React.FC = () => {
     return (
-        <div>Project</div>
+        <div style={mainDiv}>
+            <div style={inlineHeaderDiv}>
+                <div style={headerLeft}>
+                    <Title level={3} style={{ color: 'black' }}>新建项目</Title>
+                </div>
+                <div style={headerRight}>
+                    <SearchSelf />
+                </div>
+            </div>
+            <div style={inlineContentDiv}>
+                <div style={inlineContentInlineDiv}>
+                    <div style={contentLeft}>
+                        <div style={inlineContentLeft}>
+                            <div style={inlineContentInlineLeft}>
+                                <div style={{ width: '85%', height: '100%' }}>
+                                    <Title level={5} style={{ margin: '25px 0 12px 20px', color: 'black'}}>我的项目</Title>
+                                </div>
+                                {/* 开发上传组件 */}
+                                <div style={{ width: '15%', height: '100%' }}>
+                                    <ClickForm />
+                                </div>
+                            </div>
+                            <div>2</div>
+                            <div>3</div>
+                        </div>
+                    </div>
+                    <div style={contentRight}>
+                        <div style={inlineContentRight}>
+                            <div>1</div>
+                            <div>2</div>
+                            <div>3</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
