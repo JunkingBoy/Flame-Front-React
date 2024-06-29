6a2559f944a441428dff1948d147b4a9091bb717
junkingboy <junkingboy@163.com>
2024-06-29 17:43:22 +0800

    1、新增接口对接后端case接口 2、新增api请求

--- src/apis/Project.ts
+++ src/apis/Project.ts
@@ -1,6 +1,7 @@
 import { BASE_URL, axiosInstance } from "../config/HeaderInstance";
 
 import { DataContainer } from "../utils/InterfaceClass";
+import { ProjectInfo } from "../interface/ProjectInterface";
 
 async function createPro(projectName: string, projectDesc: string): Promise<DataContainer<any>> {
     let response: any = await axiosInstance.post(`${BASE_URL}/project/create`, {
@@ -11,6 +12,13 @@ async function createPro(projectName: string, projectDesc: string): Promise<Data
     return new DataContainer(response.data.code, response.data.msg, response.data.data);
 }
 
+async function getProjectInfo(): Promise<DataContainer<ProjectInfo>> {
+    let response: any = await axiosInstance.get(`${BASE_URL}/project/user/case/all`);
+
+    return new DataContainer(response.data.code, response.data.msg, response.data.data);
+}
+
 export {
-    createPro
+    createPro,
+    getProjectInfo
 }
