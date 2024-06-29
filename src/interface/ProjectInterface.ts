81446a5a750539e4118e6d99c2171bfc98d5288e
junkingboy <junkingboy@163.com>
2024-06-29 17:43:22 +0800

    1、新增接口对接后端case接口 2、新增api请求

---
+++ src/interface/ProjectInterface.ts
@@ -0,0 +1,14 @@
+interface ProjectInfo { 
+    project_name: string;
+    project_desc: string;
+    case: ProjectCaseData;
+}
+
+interface ProjectCaseData {
+    all_case: number;
+    pass_case: number;
+}
+
+export type {
+    ProjectInfo
+}
