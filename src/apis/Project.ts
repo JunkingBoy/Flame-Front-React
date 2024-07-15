44dfef590e25756bfec4c5fcbe4368bd690c6c1e
JunkingBoy <80136936+JunkingBoy@users.noreply.github.com>
2024-07-16 01:19:19 +0800

    Jun front dev (#8)
    * 添加Card组件
    
    * 1、新增接口对接后端case接口
    2、新增api请求
    
    * 删除无效引用
    
    * 1、删除不必要引用
    2、project当中新增请求后端carbox信息功能
    3、carbox当中新增处理传入的projectinfo对象设计
    
    * 1、新增cardapi从后端获取数据展示
    2、根据后端返回的数量动态渲染cardbox
    
    * 1、新增计算逻辑.计算当前项目的用例完成度
    
    * 1、新增日期选择组件
    2、新增前端api字段对接后端api
    3、对接后端card删除接口
    
    * 1、对接删除api,前端页面提供删除功能
    
    * 1、修改页面样式
    2、修改上传用例api对接后端api
    
    * 1、新增modifyPro,api对接后端api
    2、新增卡片点击修改弹出修改框
    
    * 1、修复上传栏获取不到当前项目的数值问题
    
    * 1、新增接口对接后端case接口
    2、新增api请求
    
    * 删除无效引用
    
    * 1、删除不必要引用
    2、project当中新增请求后端carbox信息功能
    3、carbox当中新增处理传入的projectinfo对象设计
    
    * 1、新增cardapi从后端获取数据展示
    2、根据后端返回的数量动态渲染cardbox
    
    * 1、新增计算逻辑.计算当前项目的用例完成度
    
    * 1、新增日期选择组件
    2、新增前端api字段对接后端api
    3、对接后端card删除接口
    
    * 1、对接删除api,前端页面提供删除功能
    
    * 1、修改页面样式
    2、修改上传用例api对接后端api
    
    * 1、新增modifyPro,api对接后端api
    2、新增卡片点击修改弹出修改框
    
    * 1、修复上传栏获取不到当前项目的数值问题
    
    * 1、新增文件上传功能.对接后端api(后端api未提供,需求需完善)
    
    * 1、修改获取用例模板api
    2、新增项目页面右侧第一块div
    
    * 1、新增数据对接类.处理后端返回数据.生成对接pie组件所需的数据
    
    * 1、修改Pie组件.准备对接后端数据
    
    * 1、新增修改对接后端api
    2、修改pie组件准备对接数据
    
    * 1、修复merge后的bug
    
    * 1、新增plan计划页面组件
    2、新增cal日历组件
    3、新增plan路由
    4、修改侧边栏菜单路由跳转
    5、新增plan样式

--- src/apis/Project.ts
+++ src/apis/Project.ts
@@ -2,14 +2,7 @@
  * @Author: Lucifer
  * @Data: Do not edit
  * @LastEditors: Lucifer
- * @LastEditTime: 2024-07-12 14:21:00
- * @Description: 
- */
-/*
- * @Author: Lucifer
- * @Data: Do not edit
- * @LastEditors: Lucifer
- * @LastEditTime: 2024-07-08 12:44:45
+ * @LastEditTime: 2024-07-16 00:52:12
  * @Description: project api
  */
 import { BASE_URL, axiosInstance } from "../config/HeaderInstance";
--- src/components/CardBox.tsx
+++ src/components/CardBox.tsx
@@ -2,7 +2,7 @@
  * @Author: Lucifer
  * @Data: Do not edit
  * @LastEditors: Lucifer
- * @LastEditTime: 2024-07-04 20:43:56
+ * @LastEditTime: 2024-07-15 18:11:05
  * @Description: 
  */
 import React, { useState } from 'react';
@@ -31,7 +31,6 @@ import { calCompletionRate } from '../utils/Calculate';
 import { pushFile } from '../apis/UpDown';
 
 const { Meta } = Card;
-const { confirm } = Modal;
 
 const mainBox: React.CSSProperties = {
     display:'flex',
@@ -159,4 +158,45 @@ const CardBox: React.FC<{ info: ProjectInfo, del: (id: number, name: string) => 
     );
 }
 
-export default CardBox;
+const PlanCard: React.FC<{ info: ProjectInfo }> = ({ info }) => {
+    const addMenu = (
+        <Menu>
+            <Menu.Item key="func">
+            新建计划
+            </Menu.Item>
+        </Menu>
+    );
+
+    const editMenu = (
+        <Menu>
+            <Menu.Item key="edit">
+            修改计划用例
+            </Menu.Item>
+        </Menu>
+    );
+
+    return (
+        <Card
+            style={{ margin: '15px 10px 15px 50px', borderRadius: '10px', width: '70%' }}
+            actions={[
+                <Dropdown overlay={addMenu}>
+                    <DiffFilled key='setting' />
+                </Dropdown>,
+                <Dropdown overlay={editMenu}>
+                    <EditOutlined key='edit' />
+                </Dropdown>
+            ]}
+            hoverable={true}
+        >
+            <Meta
+                title={info.project_name}
+                description={info.project_desc}
+            />
+        </Card>
+    )
+}
+
+export {
+    CardBox,
+    PlanCard
+};
